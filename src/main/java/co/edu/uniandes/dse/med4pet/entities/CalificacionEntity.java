076a1d94e021cdc6033e264149d5d575ea91606b
Luccas Rojas <l.rojasb@uniandes.edu.co>
2021-08-30 19:50:22 -0500

    Creacion atributos de asociacion para clase RegistroMedico y Calificaion

--- src/main/java/co/edu/uniandes/dse/med4pet/entities/CalificacionEntity.java
+++ src/main/java/co/edu/uniandes/dse/med4pet/entities/CalificacionEntity.java
@@ -3,6 +3,7 @@ package co.edu.uniandes.dse.med4pet.entities;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -13,7 +14,27 @@ import lombok.Setter;
 
 public class CalificacionEntity extends BaseEntity
 {
+	/**
+	 * Representa el creador de la calificacion.
+	 */
 	private String creador;
+	/**
+	 * Representa el puntaje de calificacion
+	 */
 	private Double puntaje;
+	/**
+	 * Representa la fecha de la calificacion.
+	 */
 	private Date fecha;
+	/**
+	 * Representa el veterinario que esta siendo calificado en la calificacion.
+	 */
+	@ManyToOne
+	private VeterinarioEntity veterinarioCalificado;
+	/**
+	 * Representa el cliente que esta siendo calificado en la calificacion.
+	 */
+	@ManyToOne
+	private ClienteEntity clienteCalificado;
+	
 }
--- src/main/java/co/edu/uniandes/dse/med4pet/entities/RegistroMedicoEntity.java
+++ src/main/java/co/edu/uniandes/dse/med4pet/entities/RegistroMedicoEntity.java
@@ -3,6 +3,7 @@ package co.edu.uniandes.dse.med4pet.entities;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToOne;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -12,7 +13,23 @@ import lombok.Setter;
 @Getter
 public class RegistroMedicoEntity extends BaseEntity 
 {
+	/**
+	 * Representa la identificacion del registro medico del veterinario.
+	 */
 	private String identificacion;
+	/**
+	 * Representa la fecha de expedicion del registro medico.
+	 */
 	private Date fechaExpedicion;
+	/**
+	 * Representa la ruta de acceso de la imagen del registro medico.
+	 */
 	private String imagen;
+	/**
+	 * Representa el veterinario que posee dicho registro medico.
+	 */
+	@OneToOne
+	VeterinarioEntity veterinario;
+	
+	
 }
