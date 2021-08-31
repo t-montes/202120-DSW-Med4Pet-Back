35b4d87094279b54528f15830f8c2a6d760f87d2
j.marinm <j.marinm@uniandes.edu.co>
2021-08-31 18:29:54 -0500

    Asociaciones para veterinario y contacto

--- src/main/java/co/edu/uniandes/dse/med4pet/entities/ContactoEntity.java
+++ src/main/java/co/edu/uniandes/dse/med4pet/entities/ContactoEntity.java
@@ -1,15 +1,49 @@
 package co.edu.uniandes.dse.med4pet.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToOne;
 
 import lombok.Getter;
 import lombok.Setter;
+import uk.co.jemos.podam.common.PodamExclude;
 
 @Entity
 @Getter
 @Setter
 public class ContactoEntity extends BaseEntity{
+	//===========================================================================
+	// Atributos
+	//===========================================================================
+
+	/**
+	 * Representa el telefono del contacto
+	 */
 	private String telefono;
+	/**
+	 * Representa el correo del contacto
+	 */
 	private String correo;
+	//===========================================================================
+	// Asociaciones
+	//===========================================================================
+
+	/**
+	 * Representa el cliente asociado al contacto.
+	 */
+	@PodamExclude
+	@OneToOne
+	private ClienteEntity cliente;
+	/**
+	 * Representa el veterinario asociado al contacto
+	 */
+	@PodamExclude
+	@OneToOne
+	private VeterinarioEntity veterinario;
+	/**
+	 * Representa la empresa asociada al contacto
+	 */
+	@PodamExclude
+	@OneToOne
+	private EmpresaConvenioEntity empresa;
 	
 }
--- src/main/java/co/edu/uniandes/dse/med4pet/entities/VeterinarioEntity.java
+++ src/main/java/co/edu/uniandes/dse/med4pet/entities/VeterinarioEntity.java
@@ -1,11 +1,17 @@
 package co.edu.uniandes.dse.med4pet.entities;
 
+import java.util.Collection;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 
 import lombok.Getter;
 import lombok.Setter;
+import uk.co.jemos.podam.common.PodamExclude;
 
 @Entity
 @Getter
@@ -13,11 +19,59 @@ import lombok.Setter;
 
 public class VeterinarioEntity extends BaseEntity{
 	
+	//===========================================================================
+	// Atributos
+	//===========================================================================
+
+	/**
+	 * Representa el Nombre del Veterinario
+	 */
 	private String nombre;
+	/**
+	 * Representa la especialidad que tiene el veterinario
+	 */
 	private String especialidad;
+	/**
+	 * Representa la experiencia de donde ha trabajado previamente un veterinario
+	 */
 	private	String[] experienciaPrevia;
+	/**
+	 * Representa la ruta del archivo para el certificado de entrenamiento del veterinario
+	 */
 	private String certificadoEntrenamiento;
+	/**
+	 * Representa la calificacion promedio del veterinario
+	 */
 	private Double calificacion;
+	
+	//===========================================================================
+	// Asociaciones
+	//===========================================================================
+
+	/**
+	 * Representa el registroMedico de un veterinario.
+	 */
+	@PodamExclude
+	@OneToOne(mappedBy ="veterinario", cascade = CascadeType.PERSIST, orphanRemoval = true)
+	private RegistroMedicoEntity registroMedico;
+	/**
+	 * Representa el contacto asociado a un veterinario.
+	 */
+	@PodamExclude
+	@OneToOne(mappedBy ="veterinario")
+	private ContactoEntity contacto;
+	/**
+	 * Representa la lista de los servicios ofrecidos por un veterinario.
+	 */
+//	@PodamExclude
+//	@ManyToMany(mappedBy="prestadores")
+//	private Collection<ServicioEntity> serviciosOfrecidos;
+	/**
+	 * Representa la lista de las calificaciones de un veterinario.
+	 */
+	@PodamExclude
+	@OneToMany(mappedBy="veterinarioCalificado")
+	private Collection<CalificacionEntity> calificaciones;
 	
 	
 }
