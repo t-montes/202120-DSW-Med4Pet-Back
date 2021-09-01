6822a45366a3f04766c8c23fb3efe5b106aeb31d
Daniel Villar <d.villarg@uniandes.edu.co>
2021-08-31 20:01:54 -0500

    Asociaciones Clases Cliente, EmpresaConvenio, Mascota y Servicio (Intento 1)

--- src/main/java/co/edu/uniandes/dse/med4pet/entities/ServicioEntity.java
+++ src/main/java/co/edu/uniandes/dse/med4pet/entities/ServicioEntity.java
@@ -1,9 +1,14 @@
 package co.edu.uniandes.dse.med4pet.entities;
 
+import java.util.Collection;
+
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+
 
 import lombok.Getter;
 import lombok.Setter;
+import uk.co.jemos.podam.common.PodamExclude;
 
 @Entity
 @Getter
@@ -11,4 +16,15 @@ import lombok.Setter;
 public class ServicioEntity extends BaseEntity{
 
 	private String nombre;
+	
+	
+	@PodamExclude
+	@ManyToMany
+	private Collection<VeterinarioEntity> prestadores;
+	
+	
+	@PodamExclude
+	@ManyToMany(mappedBy="serviciosTomados")
+	private Collection<CitaEntity> citas;
+	
 }
