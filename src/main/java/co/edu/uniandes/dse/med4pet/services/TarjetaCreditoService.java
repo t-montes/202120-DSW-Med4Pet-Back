0e41922cce44b5245e626703ad64dd290a05d6ae
Gabriela Cagua <g.cagua@uniandes.edu.co>
2021-09-12 16:08:14 -0500

    Tarjeta credito service

--- src/main/java/co/edu/uniandes/dse/med4pet/services/TarjetaCreditoService.java
+++ src/main/java/co/edu/uniandes/dse/med4pet/services/TarjetaCreditoService.java
@@ -4,12 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import co.edu.uniandes.dse.med4pet.entities.TarjetaCreditoEntity;
 import co.edu.uniandes.dse.med4pet.exceptions.EntityNotFoundException;
 import co.edu.uniandes.dse.med4pet.repositories.TarjetaCreditoRepository;
 
+@Service
 public class TarjetaCreditoService 
 {
 	@Autowired
