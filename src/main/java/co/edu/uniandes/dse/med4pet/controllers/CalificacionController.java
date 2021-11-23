847fe6b5821a71479f1a5426ec19847161cdc770
Luccas Rojas <l.rojasb@uniandes.edu.co>
2021-11-23 16:19:40 -0500

    Create Calificacion and Registro Medico

--- src/main/java/co/edu/uniandes/dse/med4pet/controllers/CalificacionController.java
+++ src/main/java/co/edu/uniandes/dse/med4pet/controllers/CalificacionController.java
@@ -8,9 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import co.edu.uniandes.dse.med4pet.dto.CalificacionDTO;
 import co.edu.uniandes.dse.med4pet.entities.CalificacionEntity;
@@ -39,6 +42,12 @@ public class CalificacionController
 	@ResponseStatus(code = HttpStatus.OK)
 	public CalificacionDTO findOne(@PathVariable("id") Long id) throws EntityNotFoundException {
     	CalificacionEntity calificacionEntity = calificacionService.getCalificacion(id);
+		return modelMapper.map(calificacionEntity, CalificacionDTO.class);
+	}
+	@PostMapping
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public CalificacionDTO create(@RequestBody CalificacionDTO calificacionDTO) throws EntityNotFoundException, co.edu.uniandes.dse.med4pet.exceptions.IllegalOperationException {
+		CalificacionEntity calificacionEntity = calificacionService.createCalificacion(modelMapper.map(calificacionDTO, CalificacionEntity.class));
 		return modelMapper.map(calificacionEntity, CalificacionDTO.class);
 	}
 
